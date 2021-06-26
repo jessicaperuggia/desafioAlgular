e04767fcac11de8ee825e4cbfcebe79bddb59754
Jessica Peruggia <jessica.peruggia@hotmail.com>
2021-06-26 00:01:16 -0300

    creating news card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NewscardComponent } from './components/newscard/newscard.component';
+
+import { MatCardModule} from '@angular/material/card';
+import {MatGridListModule, MatGridTile} from '@angular/material/grid-list/'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewscardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
